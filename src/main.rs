c482feba63e54db8dfe7b0c78f6b739c71692c75
poison-pwn <45178461+poison-pwn@users.noreply.github.com>
2021-07-10 23:24:56 +0530

    now uses anyhow for error handling OsStrs are now used in most places added map_arr macro to make OsStr arrays better error handling and quality overall

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,48 @@
 extern crate clap;
+use anyhow::{bail, Context, Result};
 use clap::{load_yaml, App, Arg};
+use std::ffi::OsStr;
 use std::fs::create_dir_all;
-use std::io::{self, Stderr, Write};
 use std::path::{Path, PathBuf};
-use std::process::exit;
 
-fn resolve_filename(filename: &mut PathBuf) {
-    let mut err_handle: Stderr = io::stderr();
-    const VIDEO_EXTENSIONS: [&str; 4] = ["mp4", "flv", "mkv", "avi"];
-    const MUSIC_EXTENSIONS: [&str; 3] = ["mp3", "aac", "m4a"];
-    let extension = &filename
-        .extension()
-        .unwrap_or_else(|| {
-            err_handle.write_all(b"invalid file extension\n").unwrap();
-            exit(1)
-        })
-        .to_str()
-        .unwrap();
+macro_rules! map_arr {
+    ($func:path,$($e:expr),*) => {[$($func($e),)*]}
+}
 
-    let dir_env_var = if VIDEO_EXTENSIONS.contains(extension) {
+fn resolve_filename(filename: &mut PathBuf) -> Result<()> {
+    let video_extensions = map_arr!(OsStr::new, "mp4", "flv", "mkv", "avi"); // video extensions supported
+    let music_extensions = map_arr!(OsStr::new, "mp3", "aac", "m4a");
+    let extension = filename.extension().context("invalid filename!")?;
+
+    let dir_env_var = if video_extensions.contains(&extension) {
         "VIDEOS"
-    } else if MUSIC_EXTENSIONS.contains(extension) {
+    } else if music_extensions.contains(&extension) {
         "MUSIC"
     } else {
-        err_handle
-            .write_all(format!("unsupported format {:?}\n", extension).as_bytes())
-            .unwrap();
-        exit(1);
+        bail!("unsupported format {:?}", extension);
     };
+
     let mut parent_dir: PathBuf;
     match filename.parent() {
         Some(var) => parent_dir = PathBuf::from(var),
-        None => return,
+        None => return Ok(()),
     }
     if parent_dir == Path::new("") {
-        let media_dir_string = std::env::var(dir_env_var).unwrap_or_else(|_| {
-            String::from(
-                std::env::current_dir()
-                    .unwrap_or_else(|_| {
-                        err_handle.write_all(b"current dir not found!\n").unwrap();
-                        exit(1);
-                    })
-                    .to_str()
-                    .unwrap(),
-            )
-        });
-        parent_dir = PathBuf::from(media_dir_string);
+        parent_dir = match std::env::var_os(dir_env_var) {
+            Some(media_dir) => PathBuf::from(media_dir),
+            None => std::env::current_dir()?,
+        };
     }
 
-    create_dir_all(&parent_dir).unwrap_or_else(|_| {
-        err_handle
-            .write_all(b"could not create parent dirs\n")
-            .unwrap();
-        exit(1);
-    });
+    // creating all intermediate parent directories of filename
+    create_dir_all(&parent_dir).context("could not create parent directories!")?;
     *filename = parent_dir
-        .canonicalize()
-        .unwrap()
-        .join(&*(filename.file_name().unwrap()).to_str().unwrap());
+        .canonicalize()? // resolves all intermediate symlinks and .. or . dirs
+        .join(filename.file_name().unwrap());
+    Ok(())
 }
 
-fn main() {
+fn main() -> Result<()> {
     let load_yaml = load_yaml!("../args.yaml");
     let matches = App::from(load_yaml)
         .arg(
@@ -73,6 +55,7 @@ fn main() {
         )
         .get_matches();
     let mut filename = PathBuf::from(matches.value_of("FILEPATH").unwrap());
-    resolve_filename(&mut filename);
+    resolve_filename(&mut filename)?; //  directly mutates and returns the resolved path
     println!("{}", filename.to_str().unwrap());
+    Ok(())
 }
