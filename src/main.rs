63ca5272e3f60b12ec8081c7cacdc574b15759ac
poison-pwn <45178461+poison-pwn@users.noreply.github.com>
2021-07-11 00:36:56 +0530

    moved resolve_filename function to validation.rs added colored output to some errors added comments and tiny optimizations

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,8 @@
 extern crate clap;
-use anyhow::{bail, Context, Result};
+mod validation;
+use anyhow::Result;
 use clap::{load_yaml, App, Arg};
-use std::ffi::OsStr;
-use std::fs::create_dir_all;
-use std::path::{Path, PathBuf};
-
-macro_rules! map_arr {
-    ($func:path,$($e:expr),*) => {[$($func($e),)*]}
-}
-
-fn resolve_filename(filename: &mut PathBuf) -> Result<()> {
-    let video_extensions = map_arr!(OsStr::new, "mp4", "flv", "mkv", "avi"); // video extensions supported
-    let music_extensions = map_arr!(OsStr::new, "mp3", "aac", "m4a");
-    let extension = filename.extension().context("invalid filename!")?;
-
-    let dir_env_var = if video_extensions.contains(&extension) {
-        "VIDEOS"
-    } else if music_extensions.contains(&extension) {
-        "MUSIC"
-    } else {
-        bail!("unsupported format {:?}", extension);
-    };
-
-    let mut parent_dir: PathBuf;
-    match filename.parent() {
-        Some(var) => parent_dir = PathBuf::from(var),
-        None => return Ok(()),
-    }
-    if parent_dir == Path::new("") {
-        parent_dir = match std::env::var_os(dir_env_var) {
-            Some(media_dir) => PathBuf::from(media_dir),
-            None => std::env::current_dir()?,
-        };
-    }
-
-    // creating all intermediate parent directories of filename
-    create_dir_all(&parent_dir).context("could not create parent directories!")?;
-    *filename = parent_dir
-        .canonicalize()? // resolves all intermediate symlinks and .. or . dirs
-        .join(filename.file_name().unwrap());
-    Ok(())
-}
+use std::path::PathBuf;
 
 fn main() -> Result<()> {
     let load_yaml = load_yaml!("../args.yaml");
@@ -55,7 +17,7 @@ fn main() -> Result<()> {
         )
         .get_matches();
     let mut filename = PathBuf::from(matches.value_of("FILEPATH").unwrap());
-    resolve_filename(&mut filename)?; //  directly mutates and returns the resolved path
+    validation::resolve_filename(&mut filename)?; //  directly mutates and returns the resolved path
     println!("{}", filename.to_str().unwrap());
     Ok(())
 }
